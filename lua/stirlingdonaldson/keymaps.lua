1edeec797f96195c081b122daf5eff8cc11993f3
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2026-03-03 23:01:23 +1100

    tidy keymap sections and add manual format shortcut

--- lua/stirlingdonaldson/keymaps.lua
+++ lua/stirlingdonaldson/keymaps.lua
@@ -1,22 +1,24 @@
-
 vim.g.mapleader = " "
 
 local opts = { noremap = true, silent = true }
 local map = vim.keymap.set
 
+-- General
+map("n", "<leader>w", ":w<CR>", opts)
+map("n", "<leader>q", ":q<CR>", opts)
+map("n", "<leader>Q", ":wqa<CR>", opts)
 
--- Version Control
-
-map("n", "<leader>lg", "<CMD>LazyGit<CR>",opts)
-
-
--- File Exploring
-
-
+-- File explorer and search
 map("n", "<C-F>", "<CMD>Open .<CR>", opts)
 map("n", "<leader>e", "<CMD>Oil<CR>", opts)
+map("n", "<leader>f", "<CMD>Telescope find_files<CR>", opts)
 
--- Misc
+-- LSP and formatting
+map("n", "<leader>lf", "<CMD>Format<CR>", opts)
 
+-- Version control
+map("n", "<leader>lg", "<CMD>LazyGit<CR>", opts)
 
+-- Command-line convenience
 map({ "n", "v", "x" }, ";", ":", opts)
+map({ "n" }, "<leader><leader>", ":", opts)
