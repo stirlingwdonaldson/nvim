ce7fc5751a596e47f06f21ba491148f09d8a0739
Stirling Donaldson <s4169686@student.rmit.edu.au>
2026-04-09 16:39:24 +1000

    moved some lines around!

--- init.lua
+++ init.lua
@@ -1,10 +1,3 @@
 print("running init.lua...")
 
 require("stirlingdonaldson.settings")
-
-vim.g.mapleader = " "
-
-vim.keymap.set("n", "<leader>e", ":Ex<CR>", { noremap = true, silent = true})
-vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true, silent = true})
-vim.keymap.set("n", "<leader>q", ":q<CR>", { noremap = true, silent = true})
-vim.keymap.set("n", "<leader>Q", ":qa!<CR>", { noremap = true, silent = true})
