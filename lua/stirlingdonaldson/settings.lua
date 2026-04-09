19df375f58f10bfeeb640abc1daa6b1d6abe43c2
Stirling Donaldson <s4169686@student.rmit.edu.au>
2026-04-09 16:30:28 +1000

    added settings.lua file!! might use for settings 👉👈

---
+++ lua/stirlingdonaldson/settings.lua
@@ -0,0 +1,21 @@
+print("loading settings.lua...")
+
+-- Basic Options
+vim.opt.number = true           -- Show absolute line numbers
+vim.opt.relativenumber = true   -- Show relative line numbers
+vim.opt.clipboard = "unnamedplus" -- Use system clipboard
+vim.opt.shiftwidth = 2          -- Size of an indent
+vim.opt.tabstop = 2             -- Number of spaces tabs count for
+vim.opt.expandtab = true        -- Turn tabs into spaces
+vim.opt.smartindent = true      -- Insert indents automatically
+vim.opt.wrap = false            -- Disable line wrap
+vim.opt.ignorecase = true       -- Ignore case in search patterns
+vim.opt.smartcase = true        -- Override ignorecase if search has capitals
+vim.opt.termguicolors = true    -- Enable 24-bit RGB color
+vim.opt.splitright = true       -- Vertical splits to the right
+vim.opt.splitbelow = true       -- Horizontal splits to the bottom
+vim.opt.scrolloff = 8           -- Minimal number of screen lines to keep above/below cursor
+vim.opt.signcolumn = "yes"      -- Always show signcolumn
+
+-- Appearance
+-- vim.cmd([[colorscheme darkblue]]) 
