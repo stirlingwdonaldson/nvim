237bb276f918992bdc140f47c9924aa46e60a2d1
Stirling Donaldson <s4169686@student.rmit.edu.au>
2026-04-09 19:57:04 +1000

    added line!

--- lua/stirlingdonaldson/plugins.lua
+++ lua/stirlingdonaldson/plugins.lua
@@ -2,6 +2,7 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-tree/nvim-web-devicons.git" },
 	{ src = "https://github.com/nvim-lua/plenary.nvim.git" },
   -- LSP PLUGINS
+  --
 	{ src = "https://github.com/mason-org/mason.nvim.git" },
 	{ src = "https://github.com/mason-org/mason-lspconfig.nvim.git" },
 	{ src = "https://github.com/neovim/nvim-lspconfig.git" },
