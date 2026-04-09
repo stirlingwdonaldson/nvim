09faa4f753f606c36ad23d511c19c6321212f195
Stirling Donaldson <s4169686@student.rmit.edu.au>
2026-04-09 19:46:01 +1000

    got LSP SET UP! Mason -> masonlspconfig -> lspconfig

--- lua/stirlingdonaldson/keymaps.lua
+++ lua/stirlingdonaldson/keymaps.lua
@@ -1,14 +1,18 @@
 vim.g.mapleader = " "
 
-
+-- general
 vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true, silent = true})
 vim.keymap.set("n", "<leader>q", ":q<CR>", { noremap = true, silent = true})
 vim.keymap.set("n", "<leader>Q", ":qa!<CR>", { noremap = true, silent = true})
 
+-- oil
 vim.keymap.set("n", "q", "<cmd>close<CR>")
-
 vim.keymap.set("n", "<leader>e", "<CMD>Oil --float<CR>", { noremap = true, silent = true})
 
+-- telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>F', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>F", builtin.live_grep, { desc = "Telescope live grep" })
+
+-- formatting
+vim.keymap.set("n", "<leader>lf", "<CMD>lua vim.lsp.buf.format()<CR>", { noremap = true, silent = true})
--- lua/stirlingdonaldson/plugins.lua
+++ lua/stirlingdonaldson/plugins.lua
@@ -1,49 +1,53 @@
 vim.pack.add({
-  { src = "https://github.com/nvim-tree/nvim-web-devicons.git" },
-  { src = "https://github.com/nvim-lua/plenary.nvim.git" },
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons.git" },
+	{ src = "https://github.com/nvim-lua/plenary.nvim.git" },
 
-  { src = "https://github.com/nvim-telescope/telescope.nvim.git" },
-  { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim.git"},
-  { src = "https://github.com/stevearc/oil.nvim.git"},
-  { src = "https://github.com/junegunn/fzf.vim.git"},
-  
+	{ src = "https://github.com/mason-org/mason.nvim.git" },
+	{ src = "https://github.com/mason-org/mason-lspconfig.nvim.git" },
+	{ src = "https://github.com/neovim/nvim-lspconfig.git" },
+
+	{ src = "https://github.com/nvim-telescope/telescope.nvim.git" },
+	{ src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim.git" },
+	{ src = "https://github.com/stevearc/oil.nvim.git" },
+	{ src = "https://github.com/junegunn/fzf.vim.git" },
 })
 
 require("nvim-web-devicons").setup()
 
-require('telescope').setup {
-  extensions = {
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
-    }
-  }
-}
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+require("telescope").setup({
+	extensions = {
+		fzf = {
+			fuzzy = true, -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+			-- the default case_mode is "smart_case"
+		},
+	},
+})
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
-require('telescope').load_extension('fzf')
+require("telescope").load_extension("fzf")
 
 require("oil").setup({
-  view_options = {
-    show_hidden = true,
-  },
-  delete_to_trash = true,
+	view_options = {
+		show_hidden = true,
+	},
+	delete_to_trash = true,
 
-float = {
-    padding = 1,
-    max_width = 0.5,
-    max_height = 0.5,
-    border = "rounded",
-    win_options = {
-      winblend = 5,
-    },
-  },
+	float = {
+		padding = 1,
+		max_width = 0.5,
+		max_height = 0.5,
+		border = "rounded",
+		win_options = {
+			winblend = 5,
+		},
+	},
 })
-
-
 
 -- oil bg color
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
